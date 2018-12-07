f0eb60b767d41babe920b63ede2bbe94098c69ab
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-12-07 13:47:41 +0200

    fisrt

---
+++ imsearch.php
@@ -0,0 +1,270 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - APS</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.3.1 - www.websitex5.com" />
+		<meta property="og:locale" content="ru" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://127.0.0.1:8080/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="APS" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-3-1" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-3-1" media="screen" />
+		<link rel="stylesheet" type="text/css" href="appsresources/css/style.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_08/css/custom.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-3-1-636797848859800650" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-3-1"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-3-1" data-files-version="14-0-3-1"></script>
+		<script type="text/javascript" src="appsresources/js/main.js"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Поиск - APS</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_07_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_7" class="" > <div id="imHeader_imCellStyleGraphics_7"></div><div id="imHeader_imCellStyle_7" ><div id="imHeader_imMenuObject_07"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Головна		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3.html">
+Про нас		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4.html">
+Галерея		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-6.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6.html">
+Контакти		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_07',1000)});
+$(function () {$('#imHeader_imMenuObject_07 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_07-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_07-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_pluginAppObj_08_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_8" class="" > <div id="imHeader_imCellStyleGraphics_8"></div><div id="imHeader_imCellStyle_8" ><div id="imHeader_pluginAppObj_08">
+
+	  <div class="cd-intro"><h1 class="cd-headline letters scale"><span class="cd-words-wrapper"><b class="is-visible">TopBudIF</b></span></h1></div>
+   </div></div></div></div><div id="imHeader_imObjectImage_09_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_9" class="" > <div id="imHeader_imCellStyleGraphics_9"></div><div id="imHeader_imCellStyle_9" ><img id="imHeader_imObjectImage_09" src="images/pixabay-2003068.png" title="" alt="" />
+<script type="text/javascript">
+	x5engine.boot.push(function () {
+		 x5engine.imagefx.rotate('#imHeader_imObjectImage_09', 10); 
+	});
+</script>
+</div></div></div><div id="imHeader_imMenuObject_10_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_10" class="" > <div id="imHeader_imCellStyleGraphics_10"></div><div id="imHeader_imCellStyle_10" ><div id="imHeader_imMenuObject_10"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Головна		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3.html">
+Про нас		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4.html">
+Галерея		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-6.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6.html">
+Контакти		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_10',1000)});
+$(function () {$('#imHeader_imMenuObject_10 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_10-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_10-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_pluginAppObj_11_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_11" class="" > <div id="imHeader_imCellStyleGraphics_11"></div><div id="imHeader_imCellStyle_11" ><div id="imHeader_pluginAppObj_11">
+          <!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+<a class="addthis_button_preferred_1"></a>
+<a class="addthis_button_preferred_2"></a>
+<a class="addthis_button_preferred_3"></a>
+<a class="addthis_button_preferred_4"></a>
+<a class="addthis_button_compact"></a>
+<a class="addthis_counter addthis_bubble_style"></a>
+</div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=incomedia"></script>
+<!-- AddThis Button END -->
+          <style>
+            #imHeader_pluginAppObj_11 .addthis_floating_style {
+                position: static;
+            }
+          </style>
+       </div></div></div></div><div id="imHeader_imObjectSearch_12_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_12" class="" > <div id="imHeader_imCellStyleGraphics_12"></div><div id="imHeader_imCellStyle_12" ><div id="imHeader_imObjectSearch_12"><form id="imHeader_imObjectSearch_12_form" action="imsearch.php" method="get"><fieldset><input type="text" id="imHeader_imObjectSearch_12_field" name="search" value="" /><button id="imHeader_imObjectSearch_12_button">Поиск</button></fieldset></form><script>$('#imHeader_imObjectSearch_12_button').click(function() { $(this).prop('disabled', true); setTimeout(function(){ $('#imHeader_imObjectSearch_12_button').prop('disabled', false);}, 900); $('#imHeader_imObjectSearch_12_form').submit(); return false; });</script></div></div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Головна		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3.html">
+Про нас		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-4.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4.html">
+Галерея		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-6.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-6.html">
+Контакти		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imStickyBar_imObjectTitle_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_2" class="" > <div id="imStickyBar_imCellStyleGraphics_2"></div><div id="imStickyBar_imCellStyle_2" ><div id="imStickyBar_imObjectTitle_02"><span id ="imStickyBar_imObjectTitle_02_text" >TopBudIF</span > </div></div></div></div><div id="imStickyBar_pluginAppObj_03_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_3" class="" > <div id="imStickyBar_imCellStyleGraphics_3"></div><div id="imStickyBar_imCellStyle_3" ><div id="imStickyBar_pluginAppObj_03">
+          <!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+<a class="addthis_button_preferred_1"></a>
+<a class="addthis_button_preferred_2"></a>
+<a class="addthis_button_preferred_3"></a>
+<a class="addthis_button_preferred_4"></a>
+<a class="addthis_button_compact"></a>
+<a class="addthis_counter addthis_bubble_style"></a>
+</div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=incomedia"></script>
+<!-- AddThis Button END -->
+          <style>
+            #imStickyBar_pluginAppObj_03 .addthis_floating_style {
+                position: static;
+            }
+          </style>
+       </div></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Результаты поиска</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imTextObject_05_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_5" class="" > <div id="imFooter_imCellStyleGraphics_5"></div><div id="imFooter_imCellStyle_5" ><div id="imFooter_imTextObject_05">
+	<div class="text-tab-content"  id="imFooter_imTextObject_05_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><b><span class="fs14 cf1">HOMEPAGE</span></b></div><div style="text-align: center;"><span class="fs10">Lorem ipsum dolor sit amet, consectetur elit.<br></span></div><div style="text-align: center;"><span class="fs10">Donec commodo sapien et dapibus consequat. </span></div><div style="text-align: center;"><span class="fs10">Phasellus a sagittis massa. </span></div><div style="text-align: center;"><span class="fs10">Suspendisse potenti.</span></div><div style="text-align: center;"><span class="fs10">Duis non arcu venenatis, cursus ex.</span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imObjectTitle_12_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_12" class="" > <div id="imFooter_imCellStyleGraphics_12"></div><div id="imFooter_imCellStyle_12" ><div id="imFooter_imObjectTitle_12"><span id ="imFooter_imObjectTitle_12_text" >TopBudIF</span > </div></div></div></div><div id="imFooter_imTextObject_15_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_15" class="" > <div id="imFooter_imCellStyleGraphics_15"></div><div id="imFooter_imCellStyle_15" ><div id="imFooter_imTextObject_15">
+	<div class="text-tab-content"  id="imFooter_imTextObject_15_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><b class="cf1">ABOUT</b></div><div style="text-align: center;"><span class="fs10">Lorem ipsum dolor sit amet, &nbsp;adipiscing elit.<br></span></div><div style="text-align: center;"><span class="fs10">Donec commodo et dapibus consequat. </span></div><div style="text-align: center;"><span class="fs10">Phasellus a sagittis massa. </span></div><div style="text-align: center;"><span class="fs10">Suspendisse potenti.</span></div><div style="text-align: center;"><span class="fs10">Duis non arcu venenatis, cursus ex.</span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imTextObject_17_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_17" class="" > <div id="imFooter_imCellStyleGraphics_17"></div><div id="imFooter_imCellStyle_17" ><div id="imFooter_imTextObject_17">
+	<div class="text-tab-content"  id="imFooter_imTextObject_17_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"><b class="cf1">QUALITY</b></div><div style="text-align: center;"><span class="fs10">Lorem ipsum dolor sit amet, &nbsp;adipiscing elit.<br></span></div><div style="text-align: center;"><span class="fs10">Donec commodo et dapibus consequat. </span></div><div style="text-align: center;"><span class="fs10">Phasellus a sagittis massa. </span></div><div style="text-align: center;"><span class="fs10">Suspendisse potenti.</span></div><div style="text-align: center;"><span class="fs10">Duis non arcu venenatis, cursus ex.</span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		<script type="text/javascript" src="cart/x5cart.js?14-0-3-1-636797848859800650"></script>
+
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
